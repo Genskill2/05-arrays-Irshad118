a449a3ebfa9264c7ea08c00959ad03a001227a6e
Irshad118 <84716726+Irshad118@users.noreply.github.com>
2021-06-19 02:08:00 +0700

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,32 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int maximum(int list[],int num) {
+ int i = list[0];
+ for (int j=0;j<num;j++) {
+     if (list[j]>i) {
+        i=list[j];
+        }
+   }
+  return i;
+ }
+
+
+int min(int list[],int num) {
+ int i = list[0];
+ for (int j=0;j<num;j++) {
+     if (list[j]<i) {
+        i=list[j];
+        }
+   }
+  return i;
+ }
+
+
+float average (int list[],int n) {
+   int i = 0;
+   for (int j=0;j<n;j++) {
+        i = i+list[j];
+        }
+  float a = (float)i/n;
+  return a;
+  } 
